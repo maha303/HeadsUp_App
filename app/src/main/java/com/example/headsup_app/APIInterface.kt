dae4d02c4ee94e2faf1af85cb09856518cb96a27
maha303 <86849690+maha303@users.noreply.github.com>
2021-10-12 00:01:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/headsup_app/APIInterface.kt
@@ -0,0 +1,22 @@
+package com.example.headsup_app
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface APIInterface {
+    @GET("/celebrities/")
+    fun getCelebrities(): Call<ArrayList<Celebrity>>
+
+    @POST("/celebrities/")
+    fun addCelebrity(@Body celebrityData: Celebrity): Call<Celebrity>
+
+    @GET("/celebrities/{id}")
+    fun getCelebrity(@Path("id") id: Int): Call<Celebrity>
+
+    // PUT replaces the full object (use PATCH to change individual fields)
+    @PUT("/celebrities/{id}")
+    fun updateCelebrity(@Path("id") id: Int, @Body celebrityData: Celebrity): Call<Celebrity>
+
+    @DELETE("/celebrities/{id}")
+    fun deleteCelebrity(@Path("id") id: Int): Call<Void>
+}
